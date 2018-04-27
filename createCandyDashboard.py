c901c1f90dd9a8115bda253ab57df77e65d06617
teqonix <teqonix@live.com>
2018-04-27 06:54:17 +0100

    Initial HTML page rendered w/ Dash

--- createCandyDashboard.py
+++ createCandyDashboard.py
@@ -3,13 +3,47 @@
 from plotly.offline import plot
 from candytestplotly import generateCandyPlots
 import datetime
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
-candyPlotter = generateCandyPlots()
+app = dash.Dash()
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
 
-start_date=datetime.date(2018,7,1)
-end_date=datetime.date(2018,7,25)
+markdown_text = '''
+# COBI Candy Tracker
+This is a dashboard showing our candy usage in the Business Intelligence Department.  Refreshed every 5 minutes.
+'''
 
-#candyheatmap = candyPlotter.createCandyHeatmap(date_from=start_date, date_to=end_date, start_hour=6, end_hour=18)
-candyweeklyusage = candyPlotter.createCandyWeeklyBarChart(4)
-#plot(candyheatmap, filename='candy-heatmap.html')
-plot(candyweeklyusage, filename='candyweeklyusage.html')
+if __name__ == '__main__':
+    candyPlotter = generateCandyPlots()
+
+    start_date=datetime.date(2018,4,18)
+    end_date=datetime.date(2018,4,26)
+    
+    candyheatmap = candyPlotter.createCandyHeatmap(date_from=start_date, date_to=end_date, start_hour=6, end_hour=18)
+    candyweeklyusage = candyPlotter.createCandyWeeklyBarChart(4)
+    
+    app.layout = html.Div(children=[
+    dcc.Markdown(children=markdown_text),
+
+    html.Div(children=
+             dcc.Graph(     
+                     id='heatmap',
+                     figure=candyheatmap
+                     )
+            ,style={'height': '360px', 'width': '70%', 'display': 'inline-block'}
+            )
+    ,html.Div(children=[
+            dcc.Graph(
+                    id='weekly candy activity'
+                    ,figure = candyweeklyusage
+                    )
+            ],style={'height': '360px', 'width': '29%', 'display': 'inline-block'})
+    
+    ]) 
+    
+    #plot(candyheatmap, filename='candy-heatmap.html')
+    #plot(candyweeklyusage, filename='candyweeklyusage.html')
+    app.run_server()
