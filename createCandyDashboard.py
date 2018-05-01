821795e940bb29d2ff07b933983d6cfe96ba011c
teqonix <teqonix@live.com>
2018-04-30 19:36:34 -0600

    Added function to return dashboard elements

--- createCandyDashboard.py
+++ createCandyDashboard.py
@@ -6,18 +6,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-def create_layout():
-    candyPlotter = generateCandyPlots()
-    print("Creating dash layout..")
-    markdown_text = '''
-# COBI Candy Tracker  
-''' + str(datetime.datetime.now())
-#'''
-#This is a dashboard showing our candy usage in the Business Intelligence Department.  Refreshed every 5 minutes.'''
-
-    end_date=datetime.datetime.now()
-    start_date= (end_date + datetime.timedelta(days=-9))
-    
+def getDashboardContent(candyPlotter, start_date, end_date):
     candyheatmap = candyPlotter.createCandyHeatmap(date_from=start_date, date_to=end_date, start_hour=6, end_hour=18)
     candyweeklyusage = candyPlotter.createCandyWeeklyBarChart(9)
 
@@ -30,23 +19,48 @@ def create_layout():
 **Summary:** """ + candyHourlyTrend.get("Hourly Trend") + """  
 **Current Hour Count:** """ + str(candyHourlyTrend.get("Current Hour Candy Count")) + """  
 **Last Hour Count:** """ + str(candyHourlyTrend.get("Lag Hour Candy Count"))
-)
-        
+    )
+    
+                
+    dailyTrendImg = candyPlotter.getKPIArrow(candyDayTrend.get("Daily Trend"))
     dailyMarkdownText = ("""
 ## Daily Trend:
 **Summary:** """ + candyDayTrend.get("Daily Trend") + """  
 **Current Day Count:** """ + str(candyDayTrend.get("Current Day Candy Count")) + """  
 **Previous Day Count:** """ + str(candyDayTrend.get("Lag Day Candy Count")) 
-)
-        
-    dailyTrendImg = candyPlotter.getKPIArrow(candyDayTrend.get("Daily Trend"))
+    )
     
+    return [candyHourlyTrend #0
+            , hourlyTrendImg #1
+            , hourlyMarkdownText #2
+            , candyDayTrend #3
+            , dailyTrendImg #4 
+            , dailyMarkdownText #5 
+            , candyheatmap #6 
+            , candyweeklyusage #7 
+            ]
+    
+
+def create_layout():
+    candyPlotter = generateCandyPlots()
+    print("Creating dash layout..")
+    markdown_text = '''
+# COBI Candy Tracker  
+Last refreshed on: ''' + str(datetime.datetime.now())
+#'''
+#This is a dashboard showing our candy usage in the Business Intelligence Department.  Refreshed every 5 minutes.'''
+
+    end_date=datetime.datetime.now()
+    start_date= (end_date + datetime.timedelta(days=-9))
+
+    dashboardData = getDashboardContent(candyPlotter, start_date, end_date)
+        
     dashboard_content = html.Div(children=[    
         html.Div(children=[
                    html.Div(children=[
                         dcc.Graph(
                                 id='weekly candy activity'
-                                ,figure = candyheatmap
+                                ,figure = dashboardData[7]
                                 )
                         ,dcc.Markdown(children=markdown_text)
                     ]
@@ -56,18 +70,18 @@ def create_layout():
                          html.Div(children=
                                      dcc.Graph(     
                                              id='heatmap',
-                                             figure=candyweeklyusage
+                                             figure=dashboardData[6]
                                              )
                                     ) 
                     ],style={'width': '25%', 'display': 'inline-block'}) 
                  ,html.Div(children=[
                           html.Div(children=[
-                                  dcc.Markdown(children=dailyMarkdownText),
-                                  html.Img(src='data:image/png;base64,{}'.format(dailyTrendImg.decode()),style={'width': '150px', 'height': 'auto'})
+                                  dcc.Markdown(children=dashboardData[5]), #daily markdown text
+                                  html.Img(src='data:image/png;base64,{}'.format(dashboardData[4].decode()),style={'width': '150px', 'height': 'auto'})
                                   ])
                           ,html.Div(children=[
-                                  dcc.Markdown(children=hourlyMarkdownText),
-                                  html.Img(src='data:image/png;base64,{}'.format(hourlyTrendImg.decode()),style={'width': '150px', 'height': 'auto'})
+                                  dcc.Markdown(children=dashboardData[2]), #hourly markdown text
+                                  html.Img(src='data:image/png;base64,{}'.format(dashboardData[1].decode()),style={'width': '150px', 'height': 'auto'})
                                   ])
                           ],style={'width': '15%', 'display': 'inline-block'})
                 ])
